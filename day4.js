9f1b293bc505a3f09e43133a29b3b9a17cebbb80
Colin Juillard <colin.juillard@gmail.com>
2023-12-16 09:23:46 -0700

    Fixed day 4 part 1

--- day4.js
+++ day4.js
@@ -3,20 +3,30 @@ import { readFileLines } from "./modules/util.js"
 
 let lines = readFileLines("input/input_day4.txt");
 
-lines = ["Card   1: 95 57 30 62 11  5  9  3 72 87 | 94 72 72 72 74"];
+// lines = ["Card   1: 95 57w 30 62 11  5  9  3 72 87 | 94 72 72 72 74"];
 let sum = 0;
 for(let i = 0; i < lines.length; i++) {
     const line = lines[i];
     
     const gameInfo = line.split(":")[1];
     const numberSets = gameInfo.split("|");
-    const winningNumbers = numberSets[0].trim();
+    const winningNumbersStr = numberSets[0].trim().split(" ");
     const scratchNumbers = numberSets[1].trim().split(" ");
 
+    const winningNumbers = new Set();
+    winningNumbersStr.forEach((value) => {
+        if(value === '')
+            return;
+
+        winningNumbers.add(parseInt(value));
+    });
+    
     let gameSum = 0;
     for(let j = 0; j < scratchNumbers.length; j++) {
-        if(winningNumbers.indexOf(scratchNumbers[j]) != -1) {
-            console.log(scratchNumbers[j]);
+        const scratchNumber = scratchNumbers[j];
+        if(scratchNumber === '')
+            continue;
+        if(winningNumbers.has(parseInt(scratchNumber))) {
             if(gameSum == 0) {
                 gameSum = 1;
             } else {
@@ -24,9 +34,6 @@ for(let i = 0; i < lines.length; i++) {
             }
         }
     }
-
-    console.log(winningNumbers);
-    console.log(scratchNumbers);
     sum += gameSum;
 }
 
